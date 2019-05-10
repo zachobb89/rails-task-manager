07b6b01090f73bce792150de3e7199a0dc12d3d3
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-10 16:51:47 +0100

    CRUD Complte

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# Create
+get 'tasks/new', to: 'tasks#new', as: 'new'
+post "tasks", to: "tasks#create", as: 'create'
+
+
+# Read
+get 'tasks', to: 'tasks#index'
+get 'tasks/:id', to: 'tasks#show', as: 'task'
+# Update
+get "tasks/:id/edit", to: "tasks#edit", as: 'edit'
+patch "tasks/:id", to: "tasks#update", as: 'update'
+
+
+# Delete
+delete "tasks/:id", to: "tasks#destroy"
 end
